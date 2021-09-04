a51a23fd67d1e1261526d258d49c7e4be28918d4
Samuel Kunst <samuel.kunst@gmail.com>
2021-09-04 17:32:18 +0200

    initial commit

---
+++ config.h
@@ -0,0 +1,8 @@
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/                                 /*Update Interval*/	/*Update Signal*/
+	{"",       "~/.config/dwmblocks/scripts/time_block",	 1,		               0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " ";
+static unsigned int delimLen = 4;
---
+++ readme.md
@@ -0,0 +1,3 @@
+### Personal dwmblocks fork
+
+All scripts are included in the repo itself. Note that this assumes you clone the repo to `~/.config/dwmblocks`, although you can install it to wherever you like as long as you change the command paths in `config.h` respectively.
