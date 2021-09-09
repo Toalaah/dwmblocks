4c478403790f1ef46aa5f0a52ff88194d1d9c19c
Samuel Kunst <samuel.kunst@gmail.com>
2021-09-09 19:19:29 +0200

    add package update script

--- config.h
+++ config.h
@@ -1,8 +1,9 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/                                 /*Update Interval*/	/*Update Signal*/
-	{"",       "~/.config/dwmblocks/blocks/time",	 1,		               0},
+	{"",       "~/.config/dwmblocks/blocks/updates",	 600,	           0},
+	{"",       "~/.config/dwmblocks/blocks/time",	     1,		           0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " ";
+static char delim[] = "|";
 static unsigned int delimLen = 4;
