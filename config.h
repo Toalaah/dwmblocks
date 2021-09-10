381e62ad7afae87fd48825ae72245c8348336736
Samuel Kunst <samuel.kunst@gmail.com>
2021-09-10 20:34:19 +0200

    add new modules: time, system updates, kernel version, volume, ip info

--- config.h
+++ config.h
@@ -1,9 +1,12 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/                                 /*Update Interval*/	/*Update Signal*/
-	{"",       "~/.config/dwmblocks/blocks/updates",	 600,	           0},
-	{"",       "~/.config/dwmblocks/blocks/time",	     1,		           0},
+	{"",       "~/.config/dwmblocks/blocks/kernelversion",	 3600,	         0},
+	{"",       "~/.config/dwmblocks/blocks/network-info",	   10,	           0},
+	{"",       "~/.config/dwmblocks/blocks/volume",	         1,	             0},
+	{"",       "~/.config/dwmblocks/blocks/updates",	       600,	           0},
+	{"",       "~/.config/dwmblocks/blocks/time",	           1,		           0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = "|";
+static char delim[] = " | ";
 static unsigned int delimLen = 4;
