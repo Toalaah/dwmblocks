15d5b88e728b9d1cff28f20deaa88b9ecc792610
Samuel Kunst <samuel.kunst@gmail.com>
2021-09-11 11:49:24 +0200

    add uptime module

--- config.h
+++ config.h
@@ -1,10 +1,10 @@
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/                                 /*Update Interval*/	/*Update Signal*/
-	{"",       "~/.config/dwmblocks/blocks/kernelversion",	 3600,	         0},
-	{"",       "~/.config/dwmblocks/blocks/network-info",	   10,	           0},
-	{"",       "~/.config/dwmblocks/blocks/volume",	         1,	             0},
-	{"",       "~/.config/dwmblocks/blocks/updates",	       600,	           0},
-	{"",       "~/.config/dwmblocks/blocks/time",	           1,		           0},
+	{"",       "~/.config/dwmblocks/blocks/kernelversion",	 3600, 0},
+	{"",       "~/.config/dwmblocks/blocks/uptime",	         60,   0},
+	{"",       "~/.config/dwmblocks/blocks/network-info",	   10,	 0},
+	{"",       "~/.config/dwmblocks/blocks/volume",	         1,	   0},
+	{"",       "~/.config/dwmblocks/blocks/updates",	       600,	 0},
+	{"",       "~/.config/dwmblocks/blocks/time",	           1,		 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
