e041b0e2412c9ed82371249075f730fcebec1bf6
Samuel Kunst <samuel.kunst@gmail.com>
2021-09-30 07:50:29 +0200

    change update signal for volume module

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const Block blocks[] = {
   {"",       "~/.config/dwmblocks/blocks/uptime",          60,   0},
   {"",       "~/.config/dwmblocks/blocks/vpn-status",      5,    0},
   {"",       "~/.config/dwmblocks/blocks/network-info",    5,    0},
-  {"",       "~/.config/dwmblocks/blocks/volume",          1,    0},
+  {"",       "~/.config/dwmblocks/blocks/volume",          1,    1},
   {"",       "~/.config/dwmblocks/blocks/updates",         600,  0},
   {"",       "~/.config/dwmblocks/blocks/time",            1,    0},
 };
